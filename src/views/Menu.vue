697fb116b37770da2f6b72713c5175c2caf100e5
Duncan MacKenzie <duncan@foundryspatial.com>
2020-03-30 19:37:46 -0700

    set up vue frontend

---
+++ src/views/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="main-menu page">
+        <h1>This is the menu page</h1>
+
+        <router-link to="/">
+            Home
+        </router-link>
+
+        <img
+            alt="Vue logo"
+            src="../assets/logo.png"
+        >
+    </div>
+</template>
