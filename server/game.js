7e57f82a0ee37e45211cce12a3e50e166aff0082
Duncan MacKenzie <duncan@foundryspatial.com>
2020-04-02 20:46:05 -0700

    add events for joining/leaving rooms

--- server/game.js
+++ server/game.js
@@ -1,9 +1,56 @@
+/**
+ * broadcast number of players to a room
+ * @param  {Server} io       Server instance
+ * @param  {String} roomName name of the room to notify
+ */
+function updateRoomClients(io, roomName) {
+    io.in(roomName).clients((error, clients) => {
+        console.log('room name', roomName, 'current clients:', clients);
+        // tell the clients how many are in the room
+        io.in(roomName).emit('number of players', clients.length);
+    });
+}
+
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('a user connected to socket');
 
-        // reply
+        // reply to new connections (@TODO: delete this lol)
         io.to(socket.id).emit('foo', { color: 'red' });
         io.to(socket.id).emit('bar', { color: 'blue' });
+
+        socket.on('join room', (roomName) => {
+            console.log(socket.id, 'trying to join room', roomName);
+            io.in(roomName).clients((error, clients) => {
+                console.log('current clients:', clients);
+                if (clients.length >= 2) {
+                    console.log(`already 2 in ${roomName} the room! not joining`);
+                    io.to(socket.id).emit('room already full', roomName);
+                } else {
+                    socket.join(roomName);
+                    console.log(socket.id, 'joined room', roomName);
+                    io.to(socket.id).emit('joined room', roomName);
+                    updateRoomClients(io, roomName);
+                }
+            });
+        });
+
+        // on player disconnection, notify the rooms they were in
+        socket.on('disconnecting', () => {
+            const { rooms } = socket;
+            console.log(socket.id, 'disconnecting');
+
+            socket.on('disconnect', () => {
+                console.log('and disconnected.');
+                console.log('was in rooms: ', rooms);
+                Object.keys(rooms).forEach((roomName) => {
+                    // skip the disconnected client's own id namespace
+                    if (roomName === socket.id) return;
+                    // notify the room that someone left
+                    updateRoomClients(io, roomName);
+                    io.in(roomName).emit('player left', null);
+                });
+            });
+        });
     });
 };
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="game page">
         <h1>This is the game page</h1>
-        <h2>the <code>gameId</code> parameter is  {{ $route.params.gameId }}</h2>
+        <h2>the <code>gameId</code> parameter is  <b>{{ gameId }}</b></h2>
+        <p>there are {{ numPlayers }} in this room</p>
     </div>
 </template>
 
@@ -11,14 +12,49 @@ import io from 'socket.io-client';
 export default {
     name: 'GameView',
 
-    mounted() {
-        const socket = io(process.env.VUE_APP_SOCKET_URL || null);
-        socket.on('foo', (data) => {
+    data: () => ({
+        socket: null,
+        numPlayers: 0,
+    }),
+
+    computed: {
+        gameId() {
+            return this.$route.params.gameId;
+        },
+    },
+
+    created() {
+        // create socket connection
+        this.socket = io(process.env.VUE_APP_SOCKET_URL || null);
+
+        // mess around for debugging - @TODO: delete this
+        this.socket.on('foo', (data) => {
             console.log('foo!', data);
         });
-        socket.on('bar', (data) => {
+        this.socket.on('bar', (data) => {
             console.log('bar!', data);
         });
+
+        // handle change in number of players
+        this.socket.on('number of players', (numPlayers) => {
+            this.numPlayers = numPlayers;
+        });
+
+        // attempt to join room
+        this.socket.on('joined room', (data) => {
+            console.log('joined room!', data);
+        });
+        this.socket.emit('join room', this.gameId);
+    },
+
+    mounted() {
+        console.log('mounted');
+    },
+
+    beforeDestroy() {
+        console.log('beforeDestroy!');
+        this.socket.close();
+        console.log('closed socket');
     },
 };
 </script>
