45a728a9c782f168fc78231ec2a1f2ccc1f1283e
Duncan MacKenzie <duncan@foundryspatial.com>
2020-04-06 20:24:08 -0700

    add basic guess handling

--- server/game.js
+++ server/game.js
@@ -1,37 +1,44 @@
 /**
  * broadcast number of players to a room
  * @param  {Server} io       Server instance
- * @param  {String} roomName name of the room to notify
+ * @param  {String} roomId name of the room to notify
  */
-function updateRoomClients(io, roomName) {
-    io.in(roomName).clients((error, clients) => {
-        console.log('room name', roomName, 'current clients:', clients);
+function updateRoomClients(io, roomId) {
+    io.in(roomId).clients((error, clients) => {
+        console.log('room name', roomId, 'current clients:', clients);
         // tell the clients how many are in the room
-        io.in(roomName).emit('number of players', clients.length);
+        io.in(roomId).emit('number of players', {
+            players: clients,
+        });
     });
 }
 
 module.exports = (io) => {
     io.on('connection', (socket) => {
+        const playerId = socket.id;
         console.log('a user connected to socket');
 
-        // reply to new connections (@TODO: delete this lol)
-        io.to(socket.id).emit('foo', { color: 'red' });
-        io.to(socket.id).emit('bar', { color: 'blue' });
-
-        socket.on('join room', (roomName) => {
-            console.log(socket.id, 'trying to join room', roomName);
-            io.in(roomName).clients((error, clients) => {
+        socket.on('join room', (roomId) => {
+            console.log(socket.id, 'trying to join room', roomId);
+            io.in(roomId).clients((error, clients) => {
                 console.log('current clients:', clients);
                 if (clients.length >= 2) {
-                    console.log(`already 2 in ${roomName} the room! not joining`);
-                    io.to(socket.id).emit('room already full', roomName);
+                    console.log(`already 2 in ${roomId} the room! not joining`);
+                    io.to(socket.id).emit('room already full', roomId);
                 } else {
-                    socket.join(roomName);
-                    console.log(socket.id, 'joined room', roomName);
-                    io.to(socket.id).emit('joined room', roomName);
-                    updateRoomClients(io, roomName);
+                    socket.join(roomId);
+                    console.log(socket.id, 'joined room', roomId);
+                    io.to(socket.id).emit('joined room', roomId);
+                    updateRoomClients(io, roomId);
                 }
+            });
+        });
+
+        socket.on('guess', ({ guess, roomId }) => {
+            console.log(playerId, 'guessed', guess, 'in', roomId);
+            io.in(roomId).emit('player guessed', {
+                playerId,
+                guess,
             });
         });
 
@@ -43,12 +50,12 @@ module.exports = (io) => {
             socket.on('disconnect', () => {
                 console.log('and disconnected.');
                 console.log('was in rooms: ', rooms);
-                Object.keys(rooms).forEach((roomName) => {
+                Object.keys(rooms).forEach((roomId) => {
                     // skip the disconnected client's own id namespace
-                    if (roomName === socket.id) return;
+                    if (roomId === socket.id) return;
                     // notify the room that someone left
-                    updateRoomClients(io, roomName);
-                    io.in(roomName).emit('player left', null);
+                    updateRoomClients(io, roomId);
+                    io.in(roomId).emit('player left', null);
                 });
             });
         });
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,6 +3,42 @@
         <h1>This is the game page</h1>
         <h2>the <code>gameId</code> parameter is  <b>{{ gameId }}</b></h2>
         <p>there are {{ numPlayers }} in this room</p>
+        <p># of guesses: {{ numGuesses }}</p>
+        <p>my guess: {{ guessInput }}, opponent's: {{ opponentGuess }}</p>
+        <div
+            v-if="roomJoined && opponentId"
+            class="game-screen"
+        >
+            joined the room! {{ roomId }}<br>
+            my ID: {{ playerId }}<br>
+            opponent ID: {{ opponentId }}<br>
+
+            <hr>
+
+            <h1 class="title is-2">
+                Guesses
+            </h1>
+            <div class="guesses">
+                <div
+                    v-for="{ player, opponent } in guesses"
+                    :key="`${player}-${opponent}`"
+                    class="guess"
+                >
+                    Mine: <b>{{ player }}</b> / Theirs: <b>{{ opponent }}</b>
+                </div>
+            </div>
+
+
+            <b-field>
+                <b-input
+                    v-model="guessInput"
+                    placeholder="Your Guess"
+                    :disabled="!guessTime"
+                    :loading="!guessTime"
+                    @keyup.enter.native="sendGuess"
+                />
+            </b-field>
+        </div>
     </div>
 </template>
 
@@ -15,11 +51,35 @@ export default {
     data: () => ({
         socket: null,
         numPlayers: 0,
+        roomId: null,
+        playerId: null,
+        opponentId: null,
+        opponentGuess: '',
+        guesses: [],
+        guessInput: '',
+        guessTime: true,
     }),
 
     computed: {
         gameId() {
             return this.$route.params.gameId;
+        },
+
+        roomJoined() {
+            return this.roomId !== null;
+        },
+
+        numGuesses() {
+            return this.guesses.length;
+        },
+    },
+
+    watch: {
+        numPlayers(val, prev) {
+            console.log('player number changed');
+            if (prev > val) {
+                this.$buefy.toast.open('a player left! game over!');
+            }
         },
     },
 
@@ -27,34 +87,104 @@ export default {
         // create socket connection
         this.socket = io(process.env.VUE_APP_SOCKET_URL || null);
 
-        // mess around for debugging - @TODO: delete this
-        this.socket.on('foo', (data) => {
-            console.log('foo!', data);
-        });
-        this.socket.on('bar', (data) => {
-            console.log('bar!', data);
+        // handle change in number of players
+        this.socket.on('number of players', ({ players }) => {
+            this.numPlayers = players.length;
+            console.log('players updated:', players);
+            if (players.length > 1) {
+                // find opponent's ID
+                const opponent = players.find((id) => id !== this.playerId);
+                console.log('opponent id is:', opponent);
+                this.opponentId = opponent;
+            }
         });
 
-        // handle change in number of players
-        this.socket.on('number of players', (numPlayers) => {
-            this.numPlayers = numPlayers;
+        // on room join: start game
+        this.socket.on('joined room', (data) => {
+            console.log('joined room!', data);
+            this.roomId = data;
+            this.playerId = this.socket.id;
+        });
+        this.socket.on('room already full', () => {
+            this.errorFatal('this game already has 2 players');
+        });
+
+        // receive guess
+        this.socket.on('player guessed', ({ guess, playerId }) => {
+            console.log('a player guessed', guess, playerId);
+            if (playerId === this.opponentId) {
+                console.log('it was your opponent!');
+                this.opponentGuess = guess;
+
+            } else {
+                console.log('but this was was not opponent', 'it was', playerId);
+            }
+            this.checkGuesses();
         });
 
         // attempt to join room
-        this.socket.on('joined room', (data) => {
-            console.log('joined room!', data);
-        });
         this.socket.emit('join room', this.gameId);
-    },
-
-    mounted() {
-        console.log('mounted');
     },
 
     beforeDestroy() {
         console.log('beforeDestroy!');
         this.socket.close();
         console.log('closed socket');
+    },
+
+    methods: {
+        // display a snackbar for fatal errors and then go back to the homepage
+        errorFatal(message) {
+            // disconnect from socket
+            this.socket.close();
+            // show error message
+            this.$buefy.snackbar.open({
+                message,
+                type: 'is-danger',
+                // position: 'is-top',
+                actionText: 'Aww, man!',
+                indefinite: true,
+                onAction: () => {
+                    this.$router.push({ name: 'home' });
+                },
+            });
+        },
+
+        sendGuess() {
+            console.log('sendGuess!', this.guessInput);
+            this.guessTime = false;
+            this.socket.emit('guess', {
+                guess: this.guessInput,
+                roomId: this.roomId,
+            });
+
+            // set loading on input
+            // send it to the Server
+            // when both are received, clear loading, clear input
+            // if the guesses match, game over!
+            // if the guesses don't match, clear disabled and start over
+        },
+
+        checkGuesses() {
+            console.log('check guesses', this.guessInput, this.opponentGuess);
+
+            if (this.guessInput && this.opponentGuess) {
+
+                this.guesses.push({
+                    player: this.guessInput,
+                    opponent: this.opponentGuess,
+                });
+
+                if (this.guessInput === this.opponentGuess) {
+                    this.$buefy.toast.open(`you win! ${this.numGuesses} guesses`);
+                } else {
+                    // new round
+                    this.guessInput = '';
+                    this.opponentGuess = '';
+                    this.guessTime = true;
+                }
+            }
+        },
     },
 };
 </script>
